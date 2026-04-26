35727738c5a108c851375caf07d2a94a4d8cbb27
sseidu96 <safiatus65@gmail.com>
2026-04-26 14:18:57 -0400

    ticket 3

--- ifstatement.sh
+++ ifstatement.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-Author : Safiatu Safiatu
-Date : April 26 2026
+# Author: Safiatu Safiatu
+# Date: April 26 2026
 
 winning_number=10
 
-echo "Welcome to 1xbet! good luck !!"
+echo "Welcome to 1xbet! Good luck!!"
 sleep 3
 
-read -p "please enter a choice between between (1-1000)" USER_CHOICE
+read -p "Please enter a choice between (1-1000): " USER_CHOICE
 
-if [ $USER_CHOICE -eq $winning_number ]
-
+if [ "$USER_CHOICE" -eq "$winning_number" ]
 then
+    echo "Congratulations! You won!!!"
 
-echo "Congratulation You won !!!"
+elif [ "$USER_CHOICE" -lt "$winning_number" ]
+then
+    echo "Your choice is less than the winning number. Please try again."
+
+elif [ "$USER_CHOICE" -gt "$winning_number" ]
+then
+    echo "Please choose a lower number."
 
 else
-echo "Sorry come back when You make more money"
-
+    echo "Sorry, come back when you make more money."
 fi
