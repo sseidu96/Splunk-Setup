3f3bca590ce80d096e16662a858c068ec817f0c1
sseidu96 <safiatus65@gmail.com>
2026-04-26 14:52:31 -0400

    ticket 3

---
+++ file-check.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -f /path/to/the/file ]
+then
+    echo "file exists"
+else
+    echo "file missing"
+fi
---
+++ file1-check.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -d /path/to/the/file ]
+then
+    echo "directory exists"
+else
+    echo "directory missing"
+fi
